97b11b713339a229b935455fdcb815dfc45372bd
Damien Pereira <damien.pereira@sysentive.com>
2022-03-21 15:13:37 +0100

    Fix tests issues

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Click here now!/i);
   expect(linkElement).toBeInTheDocument();
 });
